5a9b25654e52657b55c4086c90438f6fa523597b
aveyuan <aveyuan@163.com>
2025-09-19 22:44:13 +0800

    fix

--- app0/internal/service/service.go
+++ app0/internal/service/service.go
@@ -2,8 +2,4 @@ package service
 
 import "github.com/google/wire"
 
-<<<<<<< HEAD
 var ProviderService = wire.NewSet( NewGinUserService)
-=======
-var ProviderService = wire.NewSet( NewGinUserService, NewIrisUserService)
->>>>>>> 946887a5673d2eb48b1be7a4647b1c62b912f534
--- pkg/vhttp/handele.go
+++ pkg/vhttp/handele.go
@@ -1,57 +1,31 @@
 package vhttp
 
 import (
-	"context"
 	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/kataras/iris/v12"
 )
 
 const ErrCtx = "errCtx"
 
-func SuccessHandle(ctx context.Context, data interface{}) {
-
-	// 如果是gin，则使用gin渲染
-	if c, ok := ctx.(*gin.Context); ok {
-		c.JSON(http.StatusOK, data)
-	}
-
-	// 如果是Iris，则使用Iris渲染
-	if c, ok := ctx.(iris.Context); ok {
-		c.StatusCode(http.StatusOK)
-		c.JSON(data)
-	}
+func SuccessHandle(ctx *gin.Context, data interface{}) {
+	ctx.JSON(http.StatusOK, data)
 
 }
 
-func ErrorHandle(ctx context.Context, err error) {
+func ErrorHandle(ctx *gin.Context, err error) {
 	var errData ErrData
 	if !errors.As(err, &errData) {
 		errData.Code = http.StatusInternalServerError
 		errData.Msg = "系统错误"
 		errData.Reason = err
 	}
-	// 如果是gin，则使用gin渲染
-	if c, ok := ctx.(*gin.Context); ok {
-		c.Set(ErrCtx, &errData)
-		if errData.StatusCode != nil {
-			c.JSON(*errData.StatusCode, errData)
-			return
-		}
-		c.JSON(200, errData)
+	ctx.Set(ErrCtx, &errData)
+	if errData.StatusCode != nil {
+		ctx.JSON(*errData.StatusCode, errData)
 		return
-
 	}
-
-	// 如果是iris，则使用iris渲染
-	if c, ok := ctx.(iris.Context); ok {
-		c.Values().Set(ErrCtx, errData)
-		if errData.StatusCode != nil {
-			c.StatusCode(*errData.StatusCode)
-		}
-		c.JSON(errData)
-	}
+	ctx.JSON(200, errData)
 
 }
